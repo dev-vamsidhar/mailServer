d6de8ebecf2c95f8934508b36d177f9327f0a9cd
Vamsidhar <vamsubala1@gmail.com>
2024-05-09 11:59:40 +0530

    test

--- index.js
+++ index.js
@@ -5,8 +5,34 @@ const bodyParser = require("body-parser");
 const { google } = require("googleapis");
 const nodemailer = require("nodemailer");
 const axios = require("axios");
+const cookieParser = require("cookie-parser");
 
-app.use(cors({ origin: true }));
+const whitelist = ["http://localhost:3000"];
+// const corsOptions = {
+//   credentials: true, // This is important.
+//   origin: (origin, callback) => {
+//     if (whitelist.includes(origin)) return cmalli allback(null, true);
+
+//     callback(new Error("Not allowed by CORS"));
+//   },
+// };
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin) {
+      //for bypassing postman req with  no origin
+      return callback(null, true);
+    }
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
+app.use(cookieParser());
+
 require("dotenv").config();
 var Users = [
   {
@@ -70,29 +96,35 @@ app.post("/login", function (req, res) {
   }
 });
 
-app.get("/", async (req, res) => {
+app.get("/generateaccesstoken", async (req, res) => {
   let code = req.query.code;
+  console.log(code);
+  try {
+    const oauth2Client = new google.auth.OAuth2(
+      "718457172982-6q7hfr4v6kjj85m9velhb81fud0v9ojs.apps.googleusercontent.com",
+      "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",
+      "http://localhost:3000"
+    );
+    let { tokens } = await oauth2Client.getToken(code);
 
-  const oauth2Client = new google.auth.OAuth2(
-    "718457172982-6q7hfr4v6kjj85m9velhb81fud0v9ojs.apps.googleusercontent.com",
-    "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",
-    "http://localhost:3000"
-  );
-  let { tokens } = await oauth2Client.getToken(code);
-
-  res.cookie("accessToken", tokens.access_token, {
-    maxAge: 900000,
-    httpOnly: true,
-  });
-  res.cookie("refreshToken", tokens.refresh_token, {
-    maxAge: 900000,
-    httpOnly: true,
-    domain: "http://localhost",
-  });
-  console.log(tokens);
-  res.send({
-    status: "ok",
-  });
+    res.cookie("accessToken", tokens.access_token, {
+      maxAge: 3500,
+      httpOnly: true,
+      domain: "localhost",
+    });
+    res.cookie("refreshToken", tokens.refresh_token, {
+      maxAge: 9000000,
+      httpOnly: true,
+      domain: "localhost",
+    });
+    console.log(tokens);
+    res.send({
+      status: "ok",
+    });
+  } catch (error) {
+    console.log(error);
+    res.send("something went wrong");
+  }
 });
 
 app.post("/getMessages", async (req, res) => {
@@ -100,34 +132,62 @@ app.post("/getMessages", async (req, res) => {
   console.log(body);
   var result = [];
   var emailId = body.email;
-  var accessToken = body.access_token;
-  const options = {
-    method: "get",
-    url: `https://gmail.googleapis.com/gmail/v1/users/${emailId}/messages?maxResults=2`,
-    headers: {
-      Authorization: `Bearer ${accessToken} `,
-      "Content-type": "application/json",
-    },
-  };
-  const response = await axios(options);
+  var accessToken = req.cookies.accessToken;
+  try {
+    if (accessToken) {
+      const options = {
+        method: "get",
+        url: `https://gmail.googleapis.com/gmail/v1/users/${emailId}/messages?maxResults=2`,
+        headers: {
+          Authorization: `Bearer ${accessToken} `,
+          "Content-type": "application/json",
+        },
+      };
+      const response = await axios(options);
 
-  // console.log(response.data);
-  for (let index = 0; index < response.data.messages.length; index++) {
-    const element = response.data.messages[index];
-    var id = element.id;
-    const mailgettingoptions = {
-      method: "get",
-      url: `https://gmail.googleapis.com/gmail/v1/users/${emailId}/messages/${id}`,
-      headers: {
-        Authorization: `Bearer ${accessToken} `,
-        "Content-type": "application/json",
-      },
-    };
-    var mailData = await axios(mailgettingoptions);
-    result.push(mailData.data);
+      // console.log(response.data);
+      for (let index = 0; index < response.data.messages.length; index++) {
+        const element = response.data.messages[index];
+        var id = element.id;
+        const mailgettingoptions = {
+          method: "get",
+          url: `https://gmail.googleapis.com/gmail/v1/users/${emailId}/messages/${id}`,
+          headers: {
+            Authorization: `Bearer ${accessToken} `,
+            "Content-type": "application/json",
+          },
+        };
+        var mailData = await axios(mailgettingoptions);
+        var data = {};
+        console.log(mailData.data);
+        data.snippet = mailData.data.snippet;
+        for (let i = 0; i < mailData.data.payload.headers.length; i++) {
+          const element = mailData.data.payload.headers[i];
+          if (element.name == "To") {
+            data.to = element.value;
+          }
+          if (element.name == "Subject") {
+            data.subject = element.value;
+          }
+          if (element.name == "Message-ID") {
+            if (trackData[element.value]) {
+              data.viewCount = trackData[element.value];
+            } else {
+              data.viewCount = 0;
+            }
+          }
+        }
+        data.body = mailData.data.payload.body;
+        result.push(data);
+      }
+      res.send(result);
+    } else {
+      res.send("no acccess token sent in cookies");
+    }
+  } catch (error) {
+    console.log(error);
+    res.send("please try again the authorization");
   }
-
-  res.send(result);
 });
 
 app.post("/sendMail", async (req, res) => {
@@ -137,39 +197,46 @@ app.post("/sendMail", async (req, res) => {
   var to = body.to;
   var subject = body.subject;
   var emailId = body.email;
-  var accessToken = body.access_token;
-  var refreshToken = body.refresh_token;
+  var refreshToken = req.cookies.refreshToken;
+  var accessToken = req.cookies.accessToken;
+  if (accessToken) {
+    if (refreshToken) {
+      const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          type: "OAuth2",
+          user: emailId,
+          accessToken: accessToken,
+          clientId:
+            "718457172982-6q7hfr4v6kjj85m9velhb81fud0v9ojs.apps.googleusercontent.com",
+          clientSecret: "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",
+          refreshToken: refreshToken,
+        },
+      });
+      var serverName = process.env.server;
+      var uniqueId = Date.now();
+      msgBody =
+        msgBody +
+        `<br/> <img src='${serverName}/track?id=uniqueId' style="display:none" >`;
 
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      type: "OAuth2",
-      user: emailId,
-      accessToken: accessToken,
-      clientId:
-        "718457172982-6q7hfr4v6kjj85m9velhb81fud0v9ojs.apps.googleusercontent.com",
-      clientSecret: "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",
-      refreshToken: refreshToken,
-    },
-  });
-  var serverName = process.env.server;
-  var uniqueId = Date.now();
-  msgBody =
-    msgBody +
-    `<br/> <img src='${serverName}/track?id=uniqueId' style="display:none" >`;
+      const mailOptions = {
+        from: emailId,
+        to: to,
+        subject: subject,
+        html: msgBody,
+      };
 
-  const mailOptions = {
-    from: emailId,
-    to: to,
-    subject: subject,
-    html: msgBody,
-  };
-
-  const sentMailRes = await transporter.sendMail(mailOptions);
-  console.log(sentMailRes);
-  uniqueIdMessageMapper[uniqueId] = sentMailRes.messageId;
-  /// sentMailRes.messageId contains the id of the message
-  res.send("sent email");
+      const sentMailRes = await transporter.sendMail(mailOptions);
+      console.log(sentMailRes);
+      uniqueIdMessageMapper[uniqueId] = sentMailRes.messageId;
+      /// sentMailRes.messageId contains the id of the message
+      res.send("sent email");
+    } else {
+      res.send("not recevied refreshToken");
+    }
+  } else {
+    res.send("not recevied accessToken");
+  }
 });
 
 app.get("/track", (req, res) => {
@@ -185,7 +252,7 @@ app.get("/track", (req, res) => {
   res.sendFile(__dirname + "/test.jpeg");
 });
 
-app.listen(3000, () => {
+app.listen(8080, () => {
   console.log("listening");
 });
 
